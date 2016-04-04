ee4a2031aa03dc89104042f4dd23fc543621d252
RomaRS <romars@phystech.edu>
2016-04-04 09:30:12 +0300

    python exception handling starts

---
+++ anaconda/exceptional.py
@@ -0,0 +1,17 @@
+import sys
+
+def convert(s):
+    '''Convert to an integer'''
+    # x = -1    
+	try:
+        # x = int(s)
+		return int(s)
+        # print("Conversion succeeded! x=",x)
+    except  (ValueError, TypeError) as e:  # Tuple of exception types
+        print("Conversion error: {}"\
+		       .format(str(e)),
+			   file=sys.stderr)
+		# pass # You should not normally catch IndentationError, SyntaxError, NameError
+		return -1
+		
+    return x
