7ce75688981340da4941e7645b680a7ff1958f80
RomaRS <romars@phystech.edu>
2016-04-05 10:51:46 +0300

    grouping by course

--- anaconda/test.py
+++
@@ -1,9 +0,0 @@
-from urllib import urlopen
-
-story = urlopen('http://sixty-north.com/c/t.txt').read()
-story_words = []
-for line in story.split('\n'):
-    print line
-    line_words = line.split()
-    for word in line_words:
-        story_words.append(word)
