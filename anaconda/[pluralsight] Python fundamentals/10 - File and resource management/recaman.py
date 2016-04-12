ec2f86b397c2216a14bbb8b2a49e75729f37e3fb
RomaRS <romars@phystech.edu>
2016-04-12 09:22:02 +0300

    Finished lesson 10 - File and resource management in Python

--- anaconda/[pluralsight] Python fundamentals/10 - File and resource management/recaman.py
+++ anaconda/[pluralsight] Python fundamentals/10 - File and resource management/recaman.py
@@ -21,6 +21,9 @@ def write_sequence(filename, num):
     # f.writelines("{0}\n".format(r)
     #              for r in islice(sequence(), num+1))
     # f.close()
+    with open(filename, mode='wt', encoding='utf-8') as f:
+        f.writelines("{0}\n".format(r)
+                     for r in islice(sequence(), num+1))
 
 
 if __name__=='__main__':
