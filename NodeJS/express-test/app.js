03c471c235360275b165349f456ff524fa9624ff
RomaRS <romars@phystech.edu>
2017-01-05 18:31:39 +0300

    express-test: Added simple ejs templating.

--- NodeJS/express-test/app.js
+++ NodeJS/express-test/app.js
@@ -3,39 +3,36 @@ var http = require('http');
 var path = require('path');
 var config = require('./config');
 var log = require('./libs/logger')(module);
+var methodOverride = require('method-override');
 
 var app = express();
 app.set('port', config.get('port'));
+app.engine('ejs', require('ejs-locals')); // *.ejs to be handled by ejs-locals. Adds: layout partial block.
+app.set('views', path.join(__dirname, 'templates'));
+app.set('view engine', 'ejs');
+// Middlewares (handlers w/ next callback)
+// Most come from connect framework: http://www.senchalabs.org/connect/
+app.use(express.favicon());  // /favicon.ico
+if(app.get('env') == 'development') {  // logs requests
+  app.use(express.logger({immediate: true, format: 'dev'}));  // by default (immediate: false), overrides res.end
+} else {
+  app.use(express.logger('default'));
+}
+//app.use(express.bodyParser());  // form application/json, data in req.body
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(methodOverride());
+//app.use(express.cookieParser());  // req.headers --> req.cookies
+app.use(express.session({ secret: 'your secret here' }));
 
-http.createServer(app).listen(app.get('port'), function(){
-  log.info('Express server listening on port ' + app.get('port'));
+app.use(app.router);
+app.get('/', function(req, res, next) {  // post, delete, put, ...
+  res.render("index.ejs", {
+    title: "ExpressJS"
+  });
 });
 
-// Middlewares (handler + next parameter)
-app.use(function(req, res, next) {
-  if(req.url == '/') {
-    res.end('Hello');
-  }
-  next();
-});
-
-app.use(function(req, res, next) {
-  if(req.url == '/test') {
-    res.end('Test');
-  }
-  next();  // to next middleware
-});
-
-app.use(function(req, res, next) {
-  if(req.url == '/forbidden') {
-    return next(new Error("Access denied"));
-  }
-  next();  // to next middleware
-});
-
-app.use(function(req, res, next) {  // default middleware
-  res.send(404, "Page not found");  // express extends req and res objects with new API functions
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(function(err, req, res, next) {  // middleware for errors, fires on next(Error) or throw
   if ('development' == app.get('env')) {
@@ -47,25 +44,14 @@ app.use(function(err, req, res, next) {  // middleware for errors, fires on next
 });
 
 
-// development only
-
-
 /*
  var routes = require('./routes');
  var user = require('./routes/user');
 
- // all environments
- app.set('views', path.join(__dirname, 'views'));
- app.set('view engine', 'ejs');
- app.use(express.favicon());
- app.use(express.logger('dev'));
- app.use(express.json());
- app.use(express.urlencoded());
- app.use(express.methodOverride());
- app.use(express.session({ secret: 'your secret here' }));
- app.use(app.router);
- app.use(express.static(path.join(__dirname, 'public')));
-
  app.get('/', routes.index);
  app.get('/users', user.list);
  */
+
+http.createServer(app).listen(app.get('port'), function(){
+  log.info('Express server listening on port ' + app.get('port'));
+});
